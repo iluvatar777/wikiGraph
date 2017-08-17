1d9de3ea4af5771ba65197a66b7ab393954971b7
= <matt.giesmann@gmail.com>
2017-08-17 18:28:28 -0400

    queue and better link processing

--- index.js
+++ index.js
@@ -2,13 +2,35 @@
 
 const logger = require('./logger.js');
 const processor = require("./pageProcessor.js");
-const getPage = require("./pageRetriever.js").getPage;
-const processPage = require("./pageProcessor.js").processWikiPage;
+const checkWikiPage = require("./pageProcessor.js").checkWikiPage;
+const Promise = require('bluebird');
+const Queue = require('promise-queue')
+//Queue.configure(Promise);
 
-getPage('https://sco.wikipedia.org/wiki/Main_Page')
-.then(function(result){
-	return processPage(result)
-})
-.then(function(result){
-	logger.debug(result);
-});
+let hardLimit = 15;
+
+const queue = new Queue(3, Infinity);
+const addToQueue = function(URL) {
+	hardLimit--;
+	if (hardLimit==0) {
+		return;
+	}
+
+	queue.add(checkWikiPage(URL)
+	.then(function(result){
+		logger.debug(result.links);
+		for(let i = 0; i < result.links.length; i++) {
+			addToQueue(result.links[i]);
+		}
+	}))
+}
+
+
+addToQueue('https://sco.wikipedia.org/wiki/Main_Page');
+
+/*queue.add(checkWikiPage('https://sco.wikipedia.org/wiki/Main_Page')
+	.then(function(result){
+		logger.debug(result.links);
+
+	})
+)*/
--- pageProcessor.js
+++ pageProcessor.js
@@ -1,16 +1,80 @@
 "use strict"
 
 const Promise = require('bluebird');
+const logger = require('winston');
+const getPage = require("./pageRetriever.js").getPage;
 
-const processWikiPage = function($) {
+
+const checkWikiPage = function(pageURL, domain) {
+	domain = (typeof domain !== 'undefined') ?  domain : getWikiDomain(pageURL);
+	//TODO clean URL, check if in DB, or if DB update needed.
+
+	return getPage(pageURL)
+	.then(function(result){
+		return processWikiPage(result, domain)
+	});
+	//.then(function(result){
+	//	logger.debug(result.links);
+	//});
+};
+
+const processWikiPage = function($, domain) {
+	domain = (typeof domain !== 'undefined') ?  domain : getWikiDomain($('[rel="canonical"]').first().attr('href'));
 	return new Promise(function(resolve, reject) { 
-		let results = {links: []};
+		let results = {links: [], specialLinks: [], nonDomainLinks: []};
 		$('a').each(function (i, elem) {
-			results.links[i] = $(this).attr('href');
+			const URL = $(this).attr('href');
+			if (isDomainLink(URL, domain)) {
+				if (isSpecialLink(URL, domain)) {
+					results.specialLinks.push(getFullWikiLink(URL, domain));
+				}
+				else {
+					results.links.push(getFullWikiLink(URL, domain));
+				}
+			}
+			results.nonDomainLinks.push(URL);
 		});
 		resolve(results);
 	});
 };
 
+const getWikiDomain = function(URL) {
+	if ((''+URL.split('/')[2]).includes('wikipedia.org')) {
+		return URL.split('/')[2].split('.')[0];
+    }
+	if ((''+URL.split('/')[0]).includes('wikipedia.org')) {
+		return URL.split('/')[0].split('.')[0];
+    }
+};
 
+const getFullWikiLink = function(URL, domain) {
+	if ((''+URL.split('/')[1]) == 'wiki') {
+		return 'https://' + domain + '.wikipedia.org' + URL;
+    }
+	if ((''+URL.split('/')[0]).includes('wikipedia.org')) {
+		return 'https://' + URL;
+    }
+    return URL;
+};
+
+const isDomainLink = function(URL, domain) {
+	try {
+		if (URL.split('/')[1] == 'wiki') {
+			return true;
+		}
+		if (URL.split('/')[2].split('.')[0] == domain.split('.')[0]){
+			return true;
+		}
+	}
+	catch(ex){}
+
+	return false;
+};
+
+const isSpecialLink = function(URL, domain) {
+	return getFullWikiLink(URL, domain).split('/')[4].includes(':');
+};
+
+exports.checkWikiPage = checkWikiPage;
 exports.processWikiPage = processWikiPage;
+exports.isDomainLink = isDomainLink;
